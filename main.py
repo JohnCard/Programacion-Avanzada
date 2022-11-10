90768d698ac852c58f74807ff9f299f0129984dd
JohnCard <107288337+JohnCard@users.noreply.github.com>
2022-11-10 12:03:35 -0600

    12:03 p. m.

---
+++ main.py
@@ -0,0 +1,37 @@
+class postman:
+    def __init__(self,username,timestamp,likes,dislikes,comments = []):
+        self.username = username
+        self.timestamp = timestamp
+        self.likes = likes
+        self.dislikes = dislikes
+        self.comments = comments
+        
+        def __str__(self):
+            return f'''
+            Nombre del usuario: {self.username}
+            Tiempo activo: {self.timestamp}
+            Cantidad de likes: {self.likes}
+            Dislikes: {self.dislikes}
+            Comentarios: {self.comments}
+            '''
+        def getNombre(self):
+            return f'El usuario se llama: {self.username}'
+        def getLikes(self):
+            return f'Cantidad de likes: {self.likes}'
+        def getDislikes(self):
+            return f'Cantidad de dislikes: {self.dislikes}'
+        def tiempoTranscurrido(self):
+            return f'Tiempo transcurrido desde inicio de sesión: {self.timestamp}'
+    
+    class commentedPostmen(postman):
+        def __init__(self,message,photo):
+            super().__init__(likes,comments)
+            self.likes = likes
+            self.comments = comments
+            self.message = message
+            self.photo = photo
+        def getMessage(self):
+            return f'Mensaje a mostrar: {self.message}'
+    
+    class messagePost(commentedPostmen):
+        def __init__(self,)
