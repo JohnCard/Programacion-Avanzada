2aa132b6a189c6fa333804da4d7ea7c5b94a3b8a
JohnCard <107288337+JohnCard@users.noreply.github.com>
2022-11-14 12:00:06 -0600

    11:59 a. m.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class postman:
-    def __init__(self,username,timestamp,likes,dislikes,comments = []):
+    def __init__(self,likes,comments = [],username,timestamp,dislikes):
         self.username = username
         self.timestamp = timestamp
         self.likes = likes
@@ -23,15 +23,27 @@ class postman:
         def tiempoTranscurrido(self):
             return f'Tiempo transcurrido desde inicio de sesión: {self.timestamp}'
     
-    class commentedPostmen(postman):
-        def __init__(self,message,photo):
-            super().__init__(likes,comments)
-            self.likes = likes
-            self.comments = comments
-            self.message = message
-            self.photo = photo
-        def getMessage(self):
-            return f'Mensaje a mostrar: {self.message}'
-    
-    class messagePost(commentedPostmen):
-        def __init__(self,)
+class commentedPostmen(postman):
+    def __init__(self,likes,comments,message,photo,date_post,user_post):
+        super().__init__(likes,comments)
+        self.user_post = user_post
+        self.likes = likes
+        self.comments = comments
+        self.message = message
+        self.photo = photo
+    def getMessage(self):
+        return f'Mensaje a mostrar: {self.message}'
+
+class messagePost(commentedPostmen):
+    def __init__(self,message,message_date,user_message):
+        super().__init__(message)
+        self.message_date = message_date
+        self.user_message = user_message
+    def getMessage(self):
+        return f'Mensaje a mostrar: {self.message}'
+    def __str__(self):
+        return f'''
+        Mensaje digitado: {self.message}
+        Fecha_mensaje: {self.message_date}
+        Mensaje del usuario: {self.user_message}
+        '''
