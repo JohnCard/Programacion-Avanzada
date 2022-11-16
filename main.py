f043aac279fc5b7822ce9e688db9d309815b71b2
JohnCard <107288337+JohnCard@users.noreply.github.com>
2022-11-15 23:04:21 -0600

    11:04 p. m.

--- main.py
+++ main.py
@@ -1,49 +1,42 @@
 class postman:
-    def __init__(self,likes,comments,user_name,timestamp,dislikes):
-        self.user_name = user_name
-        self.timestamp = timestamp
+    def __init__(self,likes,comments):
+        # self.user_name = user_name
+        # self.timestamp = timestamp
         self.likes = likes
-        self.dislikes = dislikes
+        # self.dislikes = dislikes
         self.comments = list(comments)
         
     def __str__(self):
-        return f'''Nombre del usuario: {self.user_name}
-Tiempo activo: {self.timestamp}
+        return f'''
 Cantidad de likes: {self.likes}
-Dislikes: {self.dislikes}
 Comentarios: {self.comments}'''
-    def getNombre(self):
-        return f'El usuario se llama: {self.user_name}'
+    # def getNombre(self):
+    #     return f'El usuario se llama: {self.user_name}'
     def getLikes(self):
         return f'Cantidad de likes: {self.likes}'
-    def getDislikes(self):
-        return f'Cantidad de dislikes: {self.dislikes}'
-    def tiempoTranscurrido(self):
-        return f'Tiempo transcurrido desde inicio de sesión: {self.timestamp}'
+    def getComments(self):
+        return f'Comentarios: {self.comments}'
+    # def tiempoTranscurrido(self):
+    #     return f'Tiempo transcurrido desde inicio de sesión: {self.timestamp}'
     
 class commentedPostmen(postman):
-    def __init__(self,likes,comments,message,photo,date_post,user_post):
+    def __init__(self,message,likes,comments):
         super().__init__(likes,comments)
-        self.date_post = date_post
-        self.user_post = user_post
-        self.likes = likes
-        self.comments = comments
+        # self.date_post = date_post
+        # self.user_post = user_post
         self.message = message
-        self.photo = photo
+        # self.photo = photo
     def getMessage(self):
         return f'Mensaje a mostrar: {self.message}'
     def getLikes(self):
         return f'Cantidad de likes: {self.likes}'
-    def getUser(self):
-        return f'El usuario se llama: {self.user_post}'
     def __str__(self):
         return f'''Su mensaje: {self.message}
-Cantidad de likes: {self.likes}
-Nombre del usuario: {self.user_post}'''
+Cantidad de likes: {self.likes}'''
 
 class messagePost(commentedPostmen):
-    def __init__(self,message,message_date,user_message):
-        super().__init__(message)
+    def __init__(self,message,likes,comments,message_date,user_message):
+        super().__init__(message,likes,comments)
         self.message_date = message_date
         self.user_message = user_message
     def getMessage(self):
@@ -56,6 +49,34 @@ Mensaje del usuario: {self.user_message}'''
 # creando un usario:
 
 cantidad_usuarios = int(input('Digite la cantidad de usuarios a digitar: '))
+usuarios_nuevos = []
+postmen_list = []
+message_postlist = []
 for i in range(cantidad_usuarios):
     name = input(f'Digite el nombre del usuario número {i+1}: ')
     no_likes = int(input('Digite la cantidad de likes: '))
+    numero_comentarios = int(input('Digite la cantidad de comentarios que desea dejar en la caja: '))
+    box_comments = []
+    for i in range(numero_comentarios):
+        comment = input(f'Digite el comentario número {i+1}: ')
+        box_comments.append(comment)
+    message = input('Digite una descripsión para su perfil: ')
+    fecha_registro = input('Digite la fecha actual para su registro: ')
+    
+    new_user = postman(no_likes,box_comments)
+    new_comented = commentedPostmen(message,no_likes,box_comments)
+    message_post = messagePost(message,no_likes,box_comments,fecha_registro,name)
+    postmen_list.append(new_comented)
+    message_postlist.append(message_post)
+    usuarios_nuevos.append(new_user)
+
+# recorriendo cada usuario nuevo:
+print('\n------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+for i in usuarios_nuevos:
+    print(i)
+print('\n------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+for i in postmen_list:
+    print(i)
+print('\n------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+for i in message_postlist:
+    print(i)
